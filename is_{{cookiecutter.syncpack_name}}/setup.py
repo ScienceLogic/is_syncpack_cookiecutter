09b16dccb0db16d4f92999ac8478b6e6199fbe0c
Carme, Pamy <pamy.carme@sciencelogic.com>
2019-11-25 16:20:46 -0400

    INT-2416: add data to the meta.json file and consume that data in the setup.py file

--- is_{{cookiecutter.syncpack_name}}/setup.py
+++ is_{{cookiecutter.syncpack_name}}/setup.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import json
+import os
+
+# getting the path of the metajson file
+metajson_path = ""
+for r, _, f in os.walk("."):
+    result_paths = [file for file in f if 'meta.json' in file]
+    if len(result_paths) > 0:
+        metajson_path = os.path.join(r, result_paths[0])
+        break
+if metajson_path:
+    with open(metajson_path) as metajson_file:
+        metadata_json = json.load(metajson_file)
 
 setup(
-    name="{{cookiecutter.syncpack_name}}",
-    version="{{cookiecutter.version}}",
-    description="{{cookiecutter.syncpack_friendly_name}}",
-    long_description="{{cookiecutter.syncpack_description}}",
-    author="{{cookiecutter.author}}",
+    name=metadata_json["name"],
+    version=metadata_json["version"],
+    description=metadata_json.get("summary", ""),
+    long_description=metadata_json.get("description", ""),
+    author=metadata_json.get("author", ""),
     packages=find_packages(exclude=["docs", "tests*"]),
     include_package_data=True,
-    url="{{cookiecutter.url_project}}",
-    install_requires=[],
+    url=metadata_json.get("home_page", ""),
+    install_requires=metadata_json.get("requires_dist", []),
     classifiers=["Development Status :: 4 - Beta", "Topic :: Other/Nonlisted Topic"],
 )
